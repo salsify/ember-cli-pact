0dcefec2bc4116c400cebb92a137388c4ac749bb
Tomas Basham <tomasbasham@gmail.com>
2019-06-22 11:48:50 +0000

    Remove `Object.assign`

--- node-tests/unit/publish-command-test.js
+++ node-tests/unit/publish-command-test.js
@@ -34,11 +34,14 @@ describe('Publish Command', function() {
       expect(true).to.equal(true, 'publishing of pacts is enabled');
     };
 
-    publishCommand.run.call(contextFor('publish'));
+    publishCommand.run.call(contextFor('publish'), {});
   });
 
   it('generates publish options in the right order: command line takes precedence', function() {
     process.env.EMBER_CLI_PACT_BROKER = 'http://localhost:1235/';
+    process.env.EMBER_CLI_PACT_BROKER_USERNAME = 'username';
+    process.env.EMBER_CLI_PACT_BROKER_PASSWORD = 'password';
+    process.env.EMBER_CLI_PACT_BROKER_TOKEN = 'abcdefg123';
 
     let result = publishCommand.getPublishOptions('great-app', '1.2.3', {
       path: 'great-app/lib/pacts',
@@ -53,9 +56,9 @@ describe('Publish Command', function() {
       pactFilesOrDirs: ['great-app/lib/pacts'],
       consumerVersion: '2.2.3',
       pactBroker: 'http://localhost:1235/',
-      pactBrokerUsername: '',
-      pactBrokerPassword: '',
-      pactBrokerToken: '',
+      pactBrokerUsername: 'username',
+      pactBrokerPassword: 'password',
+      pactBrokerToken: 'abcdefg123',
       tags: ['release']
     });
   });
