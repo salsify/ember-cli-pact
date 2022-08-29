a8df045705ee4eae22e3685b988a9d2210d91437
Calvin Lough <50885575+calvin-fb@users.noreply.github.com>
2022-08-29 08:22:58 -0400

    Update serializeMatchingRules export to fix warning

--- addon-test-support/-private/serialization/index.js
+++ addon-test-support/-private/serialization/index.js
@@ -1,3 +1,3 @@
-export { default as serializeMatchingRules } from './utils';
+export { serializeMatchingRules } from './utils';
 export { default as serializeV2 } from './v2';
 export { default as serializeV3 } from './v3';
